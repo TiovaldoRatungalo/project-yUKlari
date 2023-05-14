b2436142811674d8b9cba6ef687c1e6916c448c9
AmadeusSebastian <s2200371@student.unklab.ac.id>
2023-05-14 09:15:12 -0700

    add page profil screens

--- src/screens/Profil/style.js
+++ src/screens/Profil/style.js
@@ -22,8 +22,8 @@ export default StyleSheet.create({
   mainInfoText: (underline=false) => ({
     fontWeight: '500',
     textDecorationLine: (underline) ? 'underline' : 'none',
-    fontSize: 14,
-    color: '#000000'
+    fontSize: 15,
+    color: 'purple'
   }),
   mainInfoLeft: {
     flex: 1,
